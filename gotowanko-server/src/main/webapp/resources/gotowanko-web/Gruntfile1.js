0550575cb9066863c830786ccd54d151607f5664
msemik <michal.semik@gmail.com>
2015-05-10 12:49:29 +0200

    partial gruntfile

--- gotowanko-server/src/main/webapp/resources/gotowanko-web/Gruntfile1.js
+++ gotowanko-server/src/main/webapp/resources/gotowanko-web/Gruntfile1.js
@@ -1,37 +1,36 @@
-module.exports = function (grunt) {
+/*jshint node: true */
+
+"use strict";
+
+module.exports = function(grunt) {
+
     var proxySnippet = require('grunt-connect-proxy/lib/utils').proxyRequest;
+
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
         connect: {
             server: {
                 options: {
-                    port: 9000,
                     hostname: 'localhost',
-                    keepalive: true,
+                    port: '9000',
                     middleware: function() {
                         return [proxySnippet];
                     }
                 },
                 proxies: [
-
                     {
                         context: '/rest',
                         host: 'localhost',
                         port: 8080
                     }
-
                 ]
             }
         }
     });
 
-    grunt.loadNpmTasks('grunt-contrib-connect');
-    grunt.loadNpmTasks('grunt-connect-proxy');
-
-    grunt.registerTask('server', function (target) {
-        grunt.task.run([
-            'configureProxies:server',
-            'connect'
-        ]);
-    });
+    grunt.registerTask('server', [
+        'configureProxies:server',
+        'connect:server',
+    ]);
 };
