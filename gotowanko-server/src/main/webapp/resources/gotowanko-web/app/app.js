fd4e2ec798eed94d5759d56039e50b657a295a7c
msemik <michal.semik@gmail.com>
2015-05-11 23:36:52 +0200

    index

--- gotowanko-server/src/main/webapp/resources/gotowanko-web/app/app.js
+++ gotowanko-server/src/main/webapp/resources/gotowanko-web/app/app.js
@@ -1,13 +1,68 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('gotowankoApp', [
+var m = angular.module('gotowankoApp', [
     'ngRoute',
+    'ngCookies',
     'gotowankoApp.searchView',
     'gotowankoApp.loginView',
     'gotowankoApp.registrationView',
     'gotowankoApp.userDetailsView'
-]).
-    config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.otherwise({redirectTo: '/login'});
+])
+m.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.otherwise({redirectTo: '/search'});
+}]);
+
+m.controller('NavigationController', ['$scope', '$log', '$location', 'getUser', 'isLogged',
+    function ($scope, $log, $location, getUser, isLogged) {
+        $scope.user = getUser();
+        $scope.isLogged = isLogged;
+
+        $scope.isActive = function (viewLocation) {
+            var active = (viewLocation === $location.path());
+            $log.info($location.path() + ", active: " + active);
+            return active;
+        };
     }]);
+
+m.controller('UserStatusController', ['$scope', '$log', 'isLogged', 'getUser', 'logout1',
+    function ($scope, $log, isLogged, getUser, logout1) {
+        $scope.isLogged = isLogged;
+        $scope.user = getUser();
+        $scope.logout1 = logout1;
+
+    }]);
+
+m.factory('isLogged', ['$log', 'getUser', function ($log, getUser) {
+    return function () {
+        return getUser() != undefined
+    };
+}]);
+
+m.factory('getUser', ['$log', '$cookieStore', function ($log, $cookieStore) {
+    return function () {
+        return $cookieStore.get('current.user')
+    };
+}]);
+
+
+m.factory('logout1', ['$http', '$log', '$cookieStore', '$location', function ( $http, $log, $cookieStore, $location) {
+    return function () {
+        $log.info("Logging out");
+        $http.delete('/rest/sessions')
+            .success(function (data, status, headers, config) {
+                $cookieStore.remove('current.user');
+                $log.info(data + " " + status);
+                $scope.alerts = [];
+                $scope.alerts.push({type: 'success', msg: 'Logout successful'});
+                Session.clear();
+                $location.path('/');
+            })
+            .error(function (data, status, headers, config) {
+                $log.info(data + " " + status);
+                $scope.alerts = [];
+                $scope.alerts.push({type: 'danger', msg: data});
+            });
+    }
+
+}]);
--- gotowanko-server/src/main/webapp/resources/gotowanko-web/app/users/login/login.js
+++ gotowanko-server/src/main/webapp/resources/gotowanko-web/app/users/login/login.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('gotowankoApp.loginView', ['ngRoute', 'ab-base64', 'ui.bootstrap'])
+angular.module('gotowankoApp.loginView', ['ngRoute', 'ab-base64', 'ui.bootstrap', 'ngCookies'])
 
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/login', {
@@ -9,48 +9,54 @@ angular.module('gotowankoApp.loginView', ['ngRoute', 'ab-base64', 'ui.bootstrap'
         });
     }])
 
-    .controller('LoginController', ['$scope', '$http', '$log', 'base64', function ($scope, $http, $log, base64) {
-        $scope.gotowankoEmail = undefined;
-        $scope.alerts = [
-/*            {type: 'danger', msg: 'Oh snap! Change a few things up and try submitting again.'},
-            {type: 'success', msg: 'Well done! You successfully read this important alert message.'}*/
-        ];
+    .controller('LoginController', ['$scope', '$http', '$log', 'base64', '$cookieStore', '$location',
+        function ($scope, $http, $log, base64, $cookieStore, $location) {
+            $scope.gotowankoEmail = undefined;
+            $scope.alerts = [
+                /*            {type: 'danger', msg: 'Oh snap! Change a few things up and try submitting again.'},
+                 {type: 'success', msg: 'Well done! You successfully read this important alert message.'}*/
+            ];
 
-        $scope.closeAlert = function (index) {
-            $scope.alerts.splice(index, 1);
-        };
+            $scope.closeAlert = function (index) {
+                $scope.alerts.splice(index, 1);
+            };
 
-        $scope.gotowankoLogin = function (email, password) {
-            $scope.registrationForm.$setSubmitted();
-            $scope.alerts = [];
-            $log.info($scope);
-            $log.info("email errors:" + $scope.registrationForm.email.$error);
-            $log.info("pass errors:" + $scope.registrationForm.password.$error);
-            /*
-             for (var error in $scope.loginForm.email.$error) {
-             var alert = { type: 'danger' }
+            $scope.gotowankoLogin = function (email, password) {
+                $scope.registrationForm.$setSubmitted();
+                $scope.alerts = [];
+                $log.info($scope);
+                $log.info("email errors:" + angular.toJson($scope.registrationForm.email.$error));
+                $log.info("pass errors:" + angular.toJson($scope.registrationForm.password.$error));
 
-             }
-             }*/
+                if (!$scope.registrationForm.email.$valid || !$scope.registrationForm.password.$valid)
+                    return;
 
-            if (!$scope.registrationForm.email.$valid || !$scope.registrationForm.password.$valid)
-                return;
+                var encodedLoginData = base64.encode(email + ":" + password);
 
-            var encodedLoginData = base64.encode(email + ":" + password);
+                $log.info(email + " " + password + " " + encodedLoginData)
+                $http.defaults.headers.common['Authorization'] = 'Basic ' + encodedLoginData;
+                $http.post('/rest/sessions')
+                    .success(function (data, status, headers, config) {
+                        $http.get('/rest/users/currently_logged')
+                            .success(function (data, status, headers, config) {
+                                $scope.alerts = [];
+                                $scope.alerts.push({type: 'success', msg: 'Login successful'});
+                                $cookieStore.put('current.user', data);
+                                $scope.loginData = data;
+                                $log.info(status + ": " + data);
 
-            $log.info(email + " " + password + " " + encodedLoginData)
-            $http.defaults.headers.common['Authorization'] = 'Basic ' + encodedLoginData;
-            $http.post('/rest/sessions').
-                success(function (data, status, headers, config) {
-                    $scope.alerts = [];
-                    $scope.alerts.push({type: 'success', msg: 'Login successful'});
-                }).
-                error(function (data, status, headers, config) {
-                    $log.info(data + " " + status)
-                    $scope.alerts = [];
-                    $scope.alerts.push({type: 'danger', msg: 'Invalid email or password'});
-                });
-        };
+                            })
+                            .error(function (data, status, headers, config) {
+                                $log.warn(status + ": " + data);
+                            });
+                    })
+                    .error(function (data, status, headers, config) {
+                        $log.info(data + " " + status)
+                        $scope.alerts = [];
+                        $scope.alerts.push({type: 'danger', msg: 'Invalid email or password'});
+                    });
+            };
 
 
-    }]);
+        }]);
+
