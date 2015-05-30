3cca70fcdd02092b5c1673bb9aac2f34ff8f9920
msemik <michal.semik@gmail.com>
2015-05-30 20:22:40 +0200

    fixed save button, added validation in create recipe

--- gotowanko-server/src/main/webapp/resources/gotowanko-web/app/recipes/create/create.js
+++ gotowanko-server/src/main/webapp/resources/gotowanko-web/app/recipes/create/create.js
@@ -1,80 +1,82 @@
 'use strict';
 
-angular.module('gotowankoApp.createRecipeView', ['ngRoute'])
+var m = angular.module('gotowankoApp.createRecipeView', ['ngRoute']);
 
-    .config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.when('/recipes_create', {
-            templateUrl: '/recipes/create/create.html',
-            controller: 'CreateRecipeController'
-        });
-    }])
+m.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/recipes_create', {
+        templateUrl: '/recipes/create/create.html',
+        controller: 'CreateRecipeController'
+    });
+}])
 
-    .controller('CreateRecipeController', ['$scope', '$http', function ($scope, $http) {
-        $scope.recipeInfo = {title: "Main", approximateCost: 0, photoUrl: '', cookingTime: 0, active: true};
+m.controller('CreateRecipeController', ['$scope', '$http', function ($scope, $http) {
+    $scope.saveRecipe = function () {
 
-        $scope.recipeSteps =
-            [
-                {
-                    stepNumber: 1, photoUrl: '', videoUrl: '', timerDuration: 0, realizationTime: 0,
-                    title: 'New step', description: '', active: false, ingredients: []
-                }
-            ];
+    }
 
-        var ingredients = [];
-        $http.get('/rest/ingredients')
-            .success(function (dataResponseIngredients) {
-                $http.get('/rest/ingredients/units')
-                    .success(function (dataResponseIngredientUnits) {
-                        angular.forEach(dataResponseIngredients, function (ingredient) {
-                            ingredient.amount = 0;
-                            ingredient.units = [];
-                            ingredient.isChecked = false;
-                            angular.copy(dataResponseIngredientUnits.ingredientUnits, ingredient.units);
-                        });
-                        ingredients = dataResponseIngredients;
-                        angular.copy(ingredients, $scope.recipeSteps[0].ingredients);
+    $scope.recipeInfo = {title: "Main", photoUrl: '', active: true};
+
+    $scope.recipeSteps =
+        [
+            {
+                stepNumber: 1, photoUrl: '', videoUrl: '',  title: 'New step', description: '', active: false, ingredients: []
+            }
+        ];
+
+    var ingredients = [];
+    $http.get('/rest/ingredients')
+        .success(function (dataResponseIngredients) {
+            $http.get('/rest/ingredients/units')
+                .success(function (dataResponseIngredientUnits) {
+                    angular.forEach(dataResponseIngredients, function (ingredient) {
+                        ingredient.amount = 0;
+                        ingredient.units = [];
+                        ingredient.isChecked = false;
+                        angular.copy(dataResponseIngredientUnits.ingredientUnits, ingredient.units);
                     });
-            });
+                    ingredients = dataResponseIngredients;
+                    angular.copy(ingredients, $scope.recipeSteps[0].ingredients);
+                });
+        });
 
-        var setAllInactive = function () {
-            $scope.recipeInfo.active = false;
-            angular.forEach($scope.recipeSteps, function (recipeStep) {
-                recipeStep.active = false;
-            });
-        };
+    var setAllInactive = function () {
+        $scope.recipeInfo.active = false;
+        angular.forEach($scope.recipeSteps, function (recipeStep) {
+            recipeStep.active = false;
+        });
+    };
 
-        var addNewRecipeStep = function () {
-            var newStepNumber = $scope.recipeSteps.length + 1;
-            var copyIngredients = [];
-            angular.copy(ingredients, copyIngredients);
-            $scope.recipeSteps.push({
-                stepNumber: newStepNumber, photoUrl: '', videoUrl: '', timerDuration: 0,
-                realizationTime: 0, title: 'New step', description: '', active: true, ingredients: copyIngredients
-            });
-        };
+    var addNewRecipeStep = function () {
+        var newStepNumber = $scope.recipeSteps.length + 1;
+        var copyIngredients = [];
+        angular.copy(ingredients, copyIngredients);
+        $scope.recipeSteps.push({
+            stepNumber: newStepNumber, photoUrl: '', videoUrl: '',  title: 'New step', description: '', active: true, ingredients: copyIngredients
+        });
+    };
 
-        $scope.addRecipeStep = function () {
-            setAllInactive();
-            addNewRecipeStep();
-        };
+    $scope.addRecipeStep = function () {
+        setAllInactive();
+        addNewRecipeStep();
+    };
 
-        $scope.removeRecipeStep = function (index) {
-            setAllInactive();
-            $scope.recipeSteps.splice(index, 1);
-            for (var i = index; i < $scope.recipeSteps.length; i++) {
-                $scope.recipeSteps[i].stepNumber--;
-            }
+    $scope.removeRecipeStep = function (index) {
+        setAllInactive();
+        $scope.recipeSteps.splice(index, 1);
+        for (var i = index; i < $scope.recipeSteps.length; i++) {
+            $scope.recipeSteps[i].stepNumber--;
+        }
 
-            if (index < $scope.recipeSteps.length) {
-                $scope.recipeSteps[index].active = true;
-            } else {
-                $scope.recipeSteps[$scope.recipeSteps.length - 1].active = true;
-            }
-        };
+        if (index < $scope.recipeSteps.length) {
+            $scope.recipeSteps[index].active = true;
+        } else {
+            $scope.recipeSteps[$scope.recipeSteps.length - 1].active = true;
+        }
+    };
 
-        $scope.saveRecipe = function () {
+    $scope.saveRecipe = function () {
 
-        };
+    };
 
-        $scope.ingredientsFilter = '';
-    }]);
+    $scope.ingredientsFilter = '';
+}]);
